d8d443cfe3de93cd7b7f4cdc93a25963defcc35c
Dheeraj5604 <dheerusharma4321@gmail.com>
2025-11-28 22:22:57 +0530

    full stack

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
+
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
-    port: 8080,
+    PORT: 5173,
+    strictPort: true,
+    allowedHosts: [
+      'ronin-feed-app.onrender.com', 
+    ],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
